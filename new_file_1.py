f9d3cb2e56e159567537045658308888ba6abda0
Артур Мартенс <arturmartens@Arturs-MBP.WORKGROUP>
2021-02-27 11:33:19 +0200

    new_class_1

---
+++ new_file_1.py
@@ -0,0 +1,7 @@
+class Hero:
+    def __init__(self, name, rang: int, health, strong, equipment: list):
+        self.name = name
+        self.rang = rang
+        self.health = health
+        self.strong = strong
+        self.equipment = equipment
