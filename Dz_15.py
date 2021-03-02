0a88c2800acddef0827f9b8c875950d6cd00f7a2
Сергей Черней <mr1kuts@gmail.com>
2021-03-02 13:04:23 +0200

    Added dz

---
+++ Dz_15.py
@@ -0,0 +1,16 @@
+
+# Стягнути мій репозиторій https://github.com/Borovec-Vasil/hillel_python
+#
+#
+#     створити в ньому свою гілку
+#     добавити в цю гілку новий файл зі своїм будь яким кодом
+#     зробити Pull Request у гілку main
+#     скинути сюди посилання на Pull Request
+
+git clone https://github.com/Borovec-Vasil/hillel_python.git
+git status
+git pull origin main
+git checkout -b "Cherney_dz"
+git add Dz_15.py
+git commit -am"Added dz"
+git push origin Cherney_dz
